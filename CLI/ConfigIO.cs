c2a7fd7b086a8364ea29e2ac720f266edccb06cf
Quantumzhao <quantumzhao@protonmail.ch>
2022-08-08 17:06:56 -0400

    changed file name

--- CLI/ConfigIO.cs
+++ CLI/ConfigIO.cs
@@ -10,12 +10,17 @@ namespace Guify.CLI
 		{
 			// Use DoNotVerify in case LocalApplicationData doesn’t exist.
 			string local = Path.Combine(Environment.GetFolderPath(
-				SpecialFolder.LocalApplicationData, SpecialFolderOption.DoNotVerify), "guify");
+				SpecialFolder.LocalApplicationData, SpecialFolderOption.DoNotVerify), "guify/UI/");
 			// Ensure the directory and all its parents exist.
 			return Directory.CreateDirectory(local).FullName;
 		}
 
-		public static string CombineName(string name) => GetUIDirectory() + "/" + name;
+		public static string CombineName(string name) 
+		{
+			if (name[^4..^1] != ".xml") name += ".xml";
+
+			return GetUIDirectory() + name;
+		}
 
 		public static void AddUI(string pathWithName, bool isLink)
 		{
